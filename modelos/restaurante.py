f4791d8ebf7e41bf4e0dba2b8598d68d6e4cafbc
Leonardo Veras <andradeveras96@gmail.com>
2024-10-29 17:43:00 -0300

    Cria  um método para listar os restaurantes

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,17 +1,21 @@
 class Restaurante:
+    restaurantes = []
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+        Restaurante.restaurantes.append(self)
 
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(restaurantes) #Mostra o espaço de memória que esta alocado 
-print(restaurante_praca)
-print(restaurante_pizza)
+Restaurante.listar_restaurantes()
