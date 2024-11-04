0ec0712add0ea7cdeecd75f4a167d4a820f090df
Leonardo Veras <andradeveras96@gmail.com>
2024-11-04 19:17:28 -0300

    Adiciona o metodo de adicionar itens no cardapio

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('Praca', 'Gourmet')
-restaurante_praca.receber_avaliacao('Leo', 10)
-restaurante_praca.receber_avaliacao('Ana', 5)
-restaurante_praca.receber_avaliacao('Mari', 2)
+bebida_suco = Bebida("Suco de Melancia", 5.0 , "grande")
+prato_pao =  Prato("Pao Frances", 0.80, "Pao frances amanteigado") 
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_pao)
 
 def main():
-    Restaurante.listar_restaurantes()
+    restaurante_praca.exibir_cardapio
 
 if __name__ == "__main__":
     main()
