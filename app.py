4beb4e3c70f28ae12e198236ab0304011246edff
Leonardo Veras <andradeveras96@gmail.com>
2024-11-04 19:29:22 -0300

    adiciona um metodo abstrato para aplicar descontos

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('Praca', 'Gourmet')
 bebida_suco = Bebida("Suco de Melancia", 5.0 , "grande")
-prato_pao =  Prato("Pao Frances", 0.80, "Pao frances amanteigado") 
+bebida_suco.aplicar_desconto()
+prato_pao =  Prato("Pao Frances", 0.80, "Pao frances amanteigado")
+prato_pao.aplicar_desconto()
 restaurante_praca.adicionar_no_cardapio(bebida_suco)
 restaurante_praca.adicionar_no_cardapio(prato_pao)
+
 
 def main():
     restaurante_praca.exibir_cardapio
